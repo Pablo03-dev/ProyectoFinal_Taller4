5c7340a8ca179d508b9d54a874743dce37eb1beb
Pablo03-dev <pablo.villegasa@alumnos.arcos.cl>
2022-12-05 20:00:29 -0300

    Empezando

---
+++ Assets/ConbinacionManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConbinacionManager : MonoBehaviour
+{
+
+    //bombo = 0
+    //caja = 1
+    //platillo = 2
+    //redoble = 3
+
+    public Slot[] slots;
+    int cantidad;
+    public Sprite[] instrumentos;
+    public List<int> secuencia = new List<int>();
+    public int indiceSecuencia = 0;
+
+
+    private void Start()
+    {
+        ElegirCantidad(4);
+        ActivarSlot();
+        ElegirInstrumento();
+    }
+
+    void ElegirCantidad(int _cantidad)
+    {
+        cantidad = _cantidad;
+    }
+
+    void ActivarSlot()
+    {
+        for (int i = 0; i < cantidad; i++)
+        {
+            slots[i].gameObject.SetActive(true);
+        }
+    }
+
+    void ElegirInstrumento()
+    {
+        for (int i = 0; i < cantidad; i++)
+        {
+            int instrumentoRandom = Random.Range(0, instrumentos.Length);
+            slots[i].instrumento.sprite = instrumentos[instrumentoRandom];
+            secuencia.Add(instrumentoRandom);
+
+        }
+    }
+
+    public void ApretarInstrumento(int _instrumento)
+    {
+        if (_instrumento == secuencia[indiceSecuencia])
+        {
+            slots[indiceSecuencia].ActivarSlot();
+            indiceSecuencia++;
+            if(indiceSecuencia == cantidad)
+            {
+                CompletoSecuencia();
+            }
+        }
+    }
+
+
+    void CompletoSecuencia()
+    {
+        //resetear la secuancia, 
+        // resetear indiceSecuencia
+        // apagar los slots
+        // apagar los circulos
+        print("Completo");
+    }
+
+    void ChequearSecuencia()
+    {
+
+    }
+
+}
